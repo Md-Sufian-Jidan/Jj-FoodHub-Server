2993e87bb58ffe4cf71481f49485402762f6fad9
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-01-27 22:39:44 +0600

    User register and login functionality created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 const app = express();
 
 app.use(express.json());
@@ -7,6 +9,8 @@ app.use(cors({
     origin: process.env.APP_URL || "http://localhost:3000",
     // credentials: true
 }));
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
     console.log("Jj FOODHUB is running on port");
