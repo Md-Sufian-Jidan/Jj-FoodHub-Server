cfd82d8f05628cfb8250f70f31118fd34fbcde3b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-02-01 00:58:51 +0600

    providerMeal route api create. where provider can create, update, delete,get all his meals

--- src/modules/routes/index.ts
+++ src/modules/routes/index.ts
@@ -5,9 +5,9 @@ import { cartRouter } from "../cart/cart.route";
 import { orderRouter } from "../order/order.route";
 import { profileRouter } from "../customer/profile.route";
 import { reviewRouter } from "../customer/review.route";
+import { providerMealRouter } from "../provider/providerMeal.route";
 
 const router = Router();
-
 
 router.use("/meals", mealRouter);
 router.use('/category', categoryRouter);
@@ -15,6 +15,7 @@ router.use("/cart", cartRouter);
 router.use("/orders", orderRouter);
 router.use("/profile", profileRouter);
 router.use("/api/reviews", reviewRouter);
+router.use("/provider/meals", providerMealRouter);
 
 
 export default router;
